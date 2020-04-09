403a14db8fd8adb8e5f409435fa2b850acc159cf
Owner <ij5775wy@go.minneapolis.edu>
2020-04-08 23:24:37 -0500

    Add project files.

---
+++ ToDoList/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddToDo_Click(object sender, EventArgs e)
+        {
+            string newItem = txtNewToDo.Text.Trim();
+
+            if (!String.IsNullOrWhiteSpace(newItem))
+            {
+                if (containsTask(newItem, clsToDo.Items))
+                {
+                    MessageBox.Show("You already added that item", "Error");
+                    txtNewToDo.SelectAll();
+                }
+                else
+                {
+                    clsToDo.Items.Add(newItem);
+                    txtNewToDo.Clear();
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            List<string> finishedItems = new List<string>();
+
+            foreach (string item in clsToDo.CheckedItems)
+            {
+                finishedItems.Add(item);
+            }
+
+            foreach (string item in finishedItems)
+            {
+                clsToDo.Items.Remove(item);
+                lstDone.Items.Add(item);
+            }
+        }
+
+        private bool containsTask(string newTask, CheckedListBox.ObjectCollection currentTasks)
+        {
+            foreach (string task in currentTasks)
+            {
+                if (task.ToLower() == newTask.ToLower())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
